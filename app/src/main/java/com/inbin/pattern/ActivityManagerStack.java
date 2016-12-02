08dae08247dd10594a86bc6c45eed53d75b2cd3e
caizhenbin <caizhenbins@163.com>
2016-12-02 15:10:55 +0800

    add a UserChooseCache class

---
+++ app/src/main/java/com/inbin/pattern/ActivityManagerStack.java
@@ -0,0 +1,17 @@
+package com.inbin.pattern;
+
+/**
+ * Created by InBin on 2016/12/2.
+ */
+public class ActivityManagerStack {
+
+    //声明实例，创建Activity Stack管理栈
+    ActivityManagerStack(){
+        //初始化
+        init();
+    }
+  //初始化方法的handler
+    private void init() {
+        //TODO manager activity 生命周期
+    }
+}
