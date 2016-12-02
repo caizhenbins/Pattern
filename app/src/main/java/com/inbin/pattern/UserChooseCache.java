52c101cbe46aed9547a2513fefb5307b7f0b28c8
caizhenbin <caizhenbins@163.com>
2016-12-02 14:28:21 +0800

    add a new class

---
+++ app/src/main/java/com/inbin/pattern/UserChooseCache.java
@@ -0,0 +1,19 @@
+package com.inbin.pattern;
+
+/**
+ * Created by InBin on 2016/12/2.
+ */
+public class UserChooseCache {
+
+    //boole 用户是否第一次选择缓存模式 true：yes false：no
+    UserChooseCache(){
+
+        init();
+
+    }
+
+    private void init() {
+        //TODO handle some way
+    }
+
+}
